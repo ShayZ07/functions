725afea076d5bdabf2c2d450bdaa0eb591347dee
Shaswat Upanash <shaswatupanash2004@gmail.com>
2023-05-17 01:06:16 -0700

    factorial.c is added

---
+++ C/factorial.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int fact(int n);
+
+int main(){
+int n;
+printf("Enter your number: ");
+scanf("%d", &n);
+if(n < 0){
+	printf("Invalid number.");
+}
+else if(n == 0){
+	printf("The factorial of 0 is 1.");
+}
+else{
+	printf("The factorial of the number is: %d", fact(n));
+}
+return 0;
+}
+
+int fact(int n){
+int facto = 1, i;
+for(i=n; i>=1; i--){
+	facto = facto * i;
+}
+return facto;
+}
