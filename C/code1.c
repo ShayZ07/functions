acd525f603ed46a2226805c1ab68f9e4b915f500
Shaswat Upanash <shaswatupanash2004@gmail.com>
2023-05-18 23:11:37 -0700

    factorial of a number is added.

--- C/code1.c
+++ C/code1.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
 
+int fact(int n);
+
 int main(){
-printf("Hello World.");
+int n;
+printf("Enter your number: ");
+scanf("%d", &n);
+	if(n < 0){
+		printf("INVALID NUMBER.");
+	}
+	else if(n == 0){
+		printf("The factorial of 0 is 1.");
+	}
+	else{
+		printf("The factorial of your number is: %d", fact(n));
+	}
 return 0;
+}
+
+int fact(int n){
+int facto = 1, i;
+	for(i=n; i>=1; i--){
+		facto = facto * i;
+	}
+return facto;
 }
